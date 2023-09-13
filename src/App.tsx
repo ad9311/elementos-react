e20a533dc071bac6bf2fe9a7bb603347e1f581c1
Ángel Díaz <diazangel9311@gmail.com>
2023-09-12 21:11:01 -0500

    Remove custom code

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+import Elements from './pages/element';
 import './assets/css/App.css';
 
 function App() {
-  return null;
+  return (
+    <Routes>
+      <Route path="/" element={<Elements />} />
+      <Route path="/elements" element={<Navigate to="/" />} />
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/pages/element/Elements.tsx
@@ -0,0 +1,5 @@
+function Elements() {
+  return <h1>Elementos - Periodic Table</h1>;
+}
+
+export default Elements;
